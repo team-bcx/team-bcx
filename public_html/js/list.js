6669f2e3a4487b9a9f91e85c9ee86399e6702927
Bellox <billiabcd@gmail.com>
2022-03-13 13:40:34 +0100

    moved list

---
+++ public_html/js/list.js
@@ -0,0 +1,472 @@
+const d = {
+	"list": [
+	    		/*=================================================================================*/
+	    				{
+			"vids": [
+			],
+			"name": "Polaris",
+                       "author": "Hexagon7 and more [ProgamerrrPL]",
+                       "more": "NovumLumen, aidancode",
+                       "id": 77809031,
+                       "percentToQualify": 44,
+                       "verificationVid": "https://www.youtube.com/watch?v=k_BKHUepkxw",
+                       "key": 0
+		},
+		/*=================================================================================*/
+						{
+			"vids": [
+			],
+			"name": "SugarShock",
+                       "author": "LCynox, tcnq [Chee2y]",
+                       "more": "none",
+                       "id": 78651997,
+                       "percentToQualify": 38,
+                       "verificationVid": "https://www.youtube.com/watch?v=13gztyFnGqo",
+                       "key": 1
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+			    			    			    			    			    				{
+					"user": "Chee2y",
+					"link": "https://youtu.be/96JiFwt70dM",
+					"percent": 60,
+				},
+			],
+			"name": "The Glodern",
+                       "author": "LCynox and more [ProgamerrrPL]",
+                       "more": "GoldenGamerPL, tcnq",
+                       "id": 75312165,
+                       "percentToQualify": 49,
+                       "verificationVid": "https://www.youtube.com/watch?v=Np0qUbWMQTg",
+                       "key": 2
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+			],
+			"name": "Sirius",
+                       "author": "Hexagon7 [ProgamerrrPL]",
+                       "more": "none",
+                       "id": 73292887,
+                       "percentToQualify": 49,
+                       "verificationVid": "https://www.youtube.com/watch?v=bQJtbQxcVhw",
+                       "key": 3
+		},
+		/*=================================================================================*/
+				{
+			"vids": [
+			    			    			    			    				{
+					"user": "tcnq",
+					"link": "https://www.youtube.com/watch?v=rYIXjRk39oI",
+					"percent": 100,
+				},
+			],
+			"name": "Incomprehension",
+                       "author": "GoldenGamerPL",
+                       "more": "none",
+                       "id": 76963886,
+                       "percentToQualify": 46,
+                       "verificationVid": "https://www.youtube.com/watch?v=pDd9ScCTmdE",
+                       "key": 4
+		},
+		/*=================================================================================*/
+								{
+			"vids": [
+			    			    			    				{
+					"user": "ProgamerrrPL",
+					"link": "https://www.youtube.com/watch?v=oTWt-zZ8lS0",
+					"percent": 100,
+				},
+							    			    			    				{
+					"user": "Chee2y",
+					"link": "https://www.youtube.com/watch?v=_XBo0uhUqxM",
+					"percent": 100,
+				},
+				                			                                                    {
+                    "user": "GoldenGamerPL",
+                    "link": "https://www.youtube.com/watch?v=NIS6qyKRrk8",
+                    "percent": 100,
+                },
+			],
+			"name": "The Knightmare",
+                       "author": "LCynox",
+                       "more": "none",
+                       "id": 75924650,
+                       "percentToQualify": 52,
+                       "verificationVid": "https://www.youtube.com/watch?v=QwWV14lFEz0",
+                       "key": 5
+		},
+		/*=================================================================================*/
+				{
+			"vids": [
+			],
+			"name": "Ash",
+                       "author": "ward0 [RealAlluminati]",
+                       "more": "none",
+                       "id": 67218317,
+                       "percentToQualify": 60,
+                       "verificationVid": "https://youtu.be/Ut7W2E6LCdQ",
+                       "key": 6
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+			],
+			"name": "ConceptCircles Redux",
+                       "author": "GoldenGamerPL [ProgamerrrPL]",
+                       "more": "none",
+                       "id": 71378741,
+                       "percentToQualify": 49,
+                       "verificationVid": "https://www.youtube.com/watch?v=6Kg7jE1RXgw",
+                       "key": 7
+		},
+		/*=================================================================================*/
+						{
+			"vids": [
+			],
+			"name": "Hexa with no Gone",
+                       "author": "mxffincatt",
+                       "more": "none",
+                       "id": 78587837,
+                       "percentToQualify": 45,
+                       "verificationVid": "https://www.youtube.com/watch?v=jwO12j4y71k",
+                       "key": 8
+		},
+		/*=================================================================================*/
+				{
+			"vids": [
+			],
+			"name": "HexaStep",
+                       "author": "Hexagon7 [GoldenGamerPL]",
+                       "more": "none",
+                       "id": 78169650,
+                       "percentToQualify": 50,
+                       "verificationVid": "https://www.youtube.com/watch?v=6NPrNyJ7SnY",
+                       "key": 9
+		},
+		/*=================================================================================*/
+						{
+			"vids": [
+			],
+			"name": "Whoa",
+                       "author": "ward0 [RealAlluminati]",
+                       "more": "none",
+                       "id": 62877744,
+                       "percentToQualify": 50,
+                       "verificationVid": "https://youtu.be/9y08eHj0dOI",
+                       "key": 10
+		},
+		/*=================================================================================*/
+		{
+
+			"vids": [
+			    				{
+					"user": "aidancode",
+					"link": "https://ibb.co/16JBBrT",
+					"percent": 100,
+				},
+							    				{
+					"user": "ProgamerrrPL",
+					"link": "https://www.youtube.com/watch?v=BHAIV66cbZM",
+					"percent": 100,
+				},
+			],
+			"name": "HellStep",
+                       "author": "Hexagon7 [LCynox]",
+                       "more": "none",
+                       "id": 72304339,
+                       "percentToQualify": 65,
+                       "verificationVid": "https://www.youtube.com/watch?v=5KA-o__IALk",
+                       "key": 11
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+			    			    				{
+					"user": "ProgamerrrPL",
+					"link": "https://www.youtube.com/watch?v=rqZrpznsepI",
+					"percent": 100,
+				},
+			],
+			"name": "Golden Processing",
+                       "author": "GoldenGamerPL",
+                       "more": "none",
+                       "id": 73334185,
+                       "percentToQualify": 65,
+                       "verificationVid": "https://www.youtube.com/watch?v=tSsWQUZnBPo",
+                       "key": 12
+		},
+		/*=================================================================================*/
+				{
+			"vids": [
+			],
+			"name": "Lights out",
+                       "author": "Hexagon7 [tcnq]",
+                       "more": "none",
+                       "id": 69120849,
+                       "percentToQualify": 58,
+                       "verificationVid": "https://www.youtube.com/watch?v=6nTGrB7Mrzs",
+                       "key": 13
+		},
+		/*=================================================================================*/
+						{
+			"vids": [
+			                    			                    {
+                    "user": "GoldenGamerPL",
+                    "link": "https://www.youtube.com/watch?v=DU0WdkzpRA0",
+                    "percent": 100,
+                },
+			                                                    {
+                    "user": "ProgamerrrPL",
+                    "link": "https://www.youtube.com/watch?v=DX-xtU3ZQ6I",
+                    "percent": 100,
+                },
+			                                                    {
+                    "user": "mxffincatt",
+                    "link": "https://youtu.be/Vpvoo0l6cWY",
+                    "percent": 100,
+                },
+			],
+			"name": "in brightness",
+                       "author": "LCynox",
+                       "more": "none",
+                       "id": 77822166,
+                       "percentToQualify": 59,
+                       "verificationVid": "https://youtu.be/RKY_7seEDhI",
+                       "key": 14
+		},
+		/*=================================================================================*/
+								{
+			"vids": [
+			],
+			"name": "Aidan with no Code",
+                       "author": "mxffincatt",
+                       "more": "none",
+                       "id": 78422706,
+                       "percentToQualify": 51,
+                       "verificationVid": "https://youtu.be/a5uMZlNnY3Y",
+                       "key": 15
+		},
+		/*=================================================================================*/
+						{
+			"vids": [
+			    			    			    				{
+			    					"user": "ProgamerrrPL",
+					"link": "https://www.youtube.com/watch?v=LTuxKcl-Buc",
+					"percent": 100,
+						},
+			    			    			    				{
+			    					"user": "mxffincatt",
+					"link": "https://youtu.be/ehaw6ob5ano",
+					"percent": 100,
+						},
+									    			    		{
+			    					"user": "Chee2y",
+					"link": "https://www.youtube.com/watch?v=d25hffUCpxY",
+					"percent": 100,
+						},
+			],
+			"name": "Absoluter Hass",
+                       "author": "LCynox",
+                       "more": "none",
+                       "id": 75150212,
+                       "percentToQualify": 65,
+                       "verificationVid": "https://www.youtube.com/watch?v=w0HmSCPuOUA",
+                       "key": 16
+		},
+		/*=================================================================================*/
+						{
+			"vids": [
+			],
+			"name": "Magical Journey",
+                       "author": "GoldenGamerPL",
+                       "more": "none",
+                       "id": 72308903,
+                       "percentToQualify": 65,
+                       "verificationVid": "https://www.youtube.com/watch?v=9eDBJlMh2bM",
+                       "key": 17
+		},
+		/*=================================================================================*/
+		{
+			"vids": [ 
+			    {
+
+					"user": "ProgamerrrPL",
+					"link": "https://www.youtube.com/watch?v=MP3oCRtDXJQ",
+					"percent": 100,
+				},
+							    {
+
+					"user": "mxffincatt",
+					"link": "https://youtu.be/DuXpjjKmTdk",
+					"percent": 100,
+				},
+			],
+			"name": "Mythical Adventure",
+                       "author": "GoldenGamerPL",
+                       "more": "none",
+                       "id": 73223499,
+                       "percentToQualify": 65,
+                       "verificationVid": "https://www.youtube.com/watch?v=GbRWyFoplGs",
+                       "key": 18
+		},
+		/*=================================================================================*/
+				{
+			"vids": [
+			    	{
+					"user": "ProgamerrrPL",
+					"link": "https://www.youtube.com/watch?v=1IwKaBHM1SU",
+					"percent": 100,
+				},
+				{
+					"user": "tcnq",
+					"link": "https://youtu.be/46QYC52Vxzs",
+					"percent": 100,
+				},
+								{
+					"user": "GoldenGamerPL",
+					"link": "https://www.youtube.com/watch?v=MFneBaPVArs",
+					"percent": 100,
+				},
+								{
+					"user": "mxffincatt",
+					"link": "https://youtu.be/brKifGgqWQs",
+					"percent": 100,
+				},
+			],
+			"name": "ConceptCircles Reborn",
+                       "author": "Hexagon7 and more [LCynox]",
+                       "more": "Pumpkinest, Alluminati, LCynox",
+                       "id": 74946649,
+                       "percentToQualify": 71,
+                       "verificationVid": "https://youtu.be/ToHoBO08Wrg",
+                       "key": 19
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+			                                                    {
+                    "user": "ProgamerrrPL",
+                    "link": "https://www.youtube.com/watch?v=DX-xtU3ZQ6I",
+                    "percent": 100,
+                },
+			                                                    {
+                    "user": "mxffincatt",
+                    "link": "https://youtu.be/dzf9ld3fTjs",
+                    "percent": 100,
+                },
+			],
+			"name": "Theory of nIce",
+                       "author": "GoldenGamerPL",
+                       "more": "none",
+                       "id": 72936164,
+                       "percentToQualify": 72,
+                       "verificationVid": "https://www.youtube.com/watch?v=_YYl8kxsWL4",
+                       "key": 20
+		},
+		/*=================================================================================*/
+								{
+			"vids": [
+			    			                                  {
+                    "user": "tcnq",
+                    "link": "https://www.youtube.com/watch?v=j1dLgaf0lqo",
+                    "percent": 100,
+                },
+			],
+			"name": "L with no cynox II",
+                       "author": "mxffincatt",
+                       "more": "none",
+                       "id": 78075495,
+                       "percentToQualify": 75,
+                       "verificationVid": "https://www.youtube.com/watch?v=gG2sK29NkYc",
+                       "key": 21
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+			    			    			    				{
+					"user": "GoldenGamerPL",
+					"link": "https://www.youtube.com/watch?v=K-YamjkjziA",
+					"percent": 100,
+				},
+							    			    			    				{
+					"user": "tcnq",
+					"link": "https://www.youtube.com/watch?v=DgHO6_S4RjM",
+					"percent": 100,
+				},
+				                                                {
+                    "user": "ProgamerrrPL",
+                    "link": "https://www.youtube.com/watch?v=DX-xtU3ZQ6I",
+                    "percent": 100,
+                },
+                				                                                {
+                    "user": "mxffincatt",
+                    "link": "https://youtu.be/5o8SoqG6nIE",
+                    "percent": 100,
+                },
+
+			],
+			"name": "HexStep",
+                       "author": "Hexagon7",
+                       "more": "none",
+                       "id": 66833221,
+                       "percentToQualify": 65,
+                       "verificationVid": "https://www.youtube.com/watch?v=V1uOQJjgDtY",
+                       "key": 22
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+			],
+			"name": "Arcane16",
+                       "author": "LCynox [Antisocial]",
+                       "more": "none",
+                       "id": 69866281,
+                       "percentToQualify": 100,
+                       "verificationVid": "https://youtu.be/aXtLC4M1Ty8",
+                       "legacykey": 0
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+			    			    				{
+					"user": "GoldenGamerPL",
+					"link": "https://www.youtube.com/watch?v=jJnZ8fDgOn8",
+					"percent": 100,
+				},
+			],
+			"name": "Hass",
+                       "author": "LCynox",
+                       "more": "none",
+                       "id": 11,
+                       "percentToQualify": 100,
+                       "verificationVid": "https://www.youtube.com/watch?v=0CuW-UvhsME",
+                       "legacykey": 1
+		},
+		/*=================================================================================*/
+				{
+			"vids": [
+			    			    				{
+					"user": "ProgamerrrPL",
+					"link": "https://www.youtube.com/watch?v=BHAIV66cbZM",
+					"percent": 100,
+				},
+			],
+			"name": "Hexagon Circles",
+                       "author": "Hexagon7 [GoldenGamerPL]",
+                       "more": "none",
+                       "id": 72827027,
+                       "percentToQualify": 57,
+                       "verificationVid": "https://www.youtube.com/watch?v=gsofCYeCqX0",
+                       "legacykey": 2
+		},				
+	],
+	"version": [
+		1,
+		0,
+		0
+	]
+};
+
+const list = d.list;const version = d.version;
